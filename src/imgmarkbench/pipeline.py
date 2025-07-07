d34eaf8e7ea4c239619d3d82163a68b15be1a5c4
Alexander Akimenkov <a.akimenkov@ispras.ru>
2025-07-07 17:36:26 +0000

    [#4] remove pipeline config field from pipeline

--- src/imgmarkbench/pipeline.py
+++ src/imgmarkbench/pipeline.py
@@ -38,7 +38,6 @@ class Pipeline:
         self.attacks = attacks
         self.post_embed_metrics = [metric for metric in metrics if isinstance(metric, PostEmbedMetric)]
         self.post_extract_metrics = [metric for metric in metrics if isinstance(metric, PostExtractMetric)]
-        self.pipeline_config = pipeline_config
         self.result_path = Path(pipeline_config.result_path)
         self.aggregator = build_fanout_from_config(pipeline_config, self.result_path)
         self.result_path.mkdir(exist_ok=True, parents=True)
