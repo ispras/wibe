c528692d7ec4591c945cf47dbb1d73959d9d880f
Yakushev Aleksey <yakushev@ispras.ru>
2025-07-17 18:42:34 +0300

    [#7] Changed arwgan submodule url, some trustmark simplification

--- src/imgmarkbench/config_loader.py
+++ src/imgmarkbench/config_loader.py
@@ -1,7 +1,7 @@
 import yaml
-
+import re
 from pathlib import Path
-from typing import Union, List, Dict, Any
+from typing import Union, List, Dict, Tuple, Any
 from imgmarkbench.algorithms.base import BaseAlgorithmWrapper
 from imgmarkbench.attacks.base import BaseAttack
 from imgmarkbench.datasets.base import BaseDataset
