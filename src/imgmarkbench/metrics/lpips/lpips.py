9bb97bf613d2f1e92b18a965d02e18f58a2a7195
Yakushev Aleksey <yakushev@ispras.ru>
2025-07-18 16:15:13 +0300

    Fixed lpips

--- src/imgmarkbench/metrics/lpips/lpips.py
+++ src/imgmarkbench/metrics/lpips/lpips.py
@@ -2,6 +2,7 @@ from typing import Any
 import lpips
 from imgmarkbench.typing import TorchImg
 from imgmarkbench.metrics.base import PostEmbedMetric
+from imgmarkbench.utils import normalize_image
 
 
 class LPIPS(PostEmbedMetric):
@@ -14,5 +15,5 @@ class LPIPS(PostEmbedMetric):
         marked_img: TorchImg,
         watermark_data: Any,
     ) -> float:
-        return float(self.loss_fn(img.unsqueeze(0), marked_img.unsqueeze(0)))
+        return float(self.loss_fn(normalize_image(img), normalize_image(marked_img)))
 
