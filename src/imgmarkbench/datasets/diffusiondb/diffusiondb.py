4715dd3077d0564d0041a95fcfa1e233e76add88
Yury Markin <ustas@ispras.ru>
2025-07-21 19:37:39 +0300

    [#20] trust_remote_code=True for diffusiondb

--- src/imgmarkbench/datasets/diffusiondb/diffusiondb.py
+++ src/imgmarkbench/datasets/diffusiondb/diffusiondb.py
@@ -18,6 +18,7 @@ class DiffusionDB(BaseDataset):
             path=self.dataset_path,
             name=subset,
             cache_dir=cache_dir,
+            trust_remote_code=True,
         )["train"]
         self.skip_nsfw = skip_nsfw
         if not skip_nsfw:
