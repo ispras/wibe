ac4a61117d830b50174a396f298f194a1a80f2e0
Alexander Akimenkov <a.akimenkov@ispras.ru>
2025-07-22 20:30:39 +0000

    [#27] Added image range for DiffusionDB dataset

--- src/imgmarkbench/datasets/diffusiondb/diffusiondb.py
+++ src/imgmarkbench/datasets/diffusiondb/diffusiondb.py
@@ -11,11 +11,12 @@ class DiffusionDB(BaseDataset):
     def __init__(
         self,
         subset: str = "2m_first_5k",
-        num_images: Optional[int] = None,
+        image_range: Optional[Tuple[int]] = None,
         cache_dir: Optional[str] = None,
         skip_nsfw: bool = True,
         return_prompt: bool = False,
     ):
+        self.image_range = image_range
         self.dataset = load_dataset(
             path=self.dataset_path,
             name=subset,
@@ -27,13 +28,14 @@ class DiffusionDB(BaseDataset):
         else:
             self.len = sum(score < 1 for score in self.dataset["image_nsfw"])
 
-        if num_images is not None:
-            if self.len < num_images:
+        if self.image_range is not None:
+            images_len = (self.image_range[1] - self.image_range[0]) + 1
+            if self.len < images_len:
                 raise ValueError(
-                    f"Dataset size is {self.len}, but num_images={num_images}"
+                    f"Dataset size is {self.len}, but num_images={images_len}"
                 )
             else:
-                self.len = num_images
+                self.len = images_len
         self.return_prompt = return_prompt
 
     def __len__(self):
@@ -43,8 +45,8 @@ class DiffusionDB(BaseDataset):
         self,
     ) -> Generator[Tuple[str, Union[TorchImg, str]], None, None]:
         img_id = -1
-        for sample in self.dataset:
-            if self.skip_nsfw and sample["image_nsfw"] >= 1:
+        for idx in range(self.image_range[0], self.image_range[1] + 1, 1):
+            if self.skip_nsfw and self.dataset[idx]["image_nsfw"] >= 1:
                 continue
             img_id += 1
 
@@ -52,6 +54,6 @@ class DiffusionDB(BaseDataset):
                 break
 
             if self.return_prompt:
-                yield str(img_id), sample["prompt"]
+                yield str(img_id), self.dataset[idx]["prompt"]
             else:
-                yield str(img_id), to_tensor(sample["image"])
+                yield str(img_id), to_tensor(self.dataset[idx]["image"])
