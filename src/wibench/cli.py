8049faee143cf1c71b0f6ab0281051fd0c336c4b
Alexander Akimenkov <a.akimenkov@ispras.ru>
2026-02-26 14:48:30 +0000

    [#45] Fix

--- src/wibench/cli.py
+++ src/wibench/cli.py
@@ -1,7 +1,49 @@
 from pathlib import Path
-import sys
-import wibench
 import json
+import os
+import sys
+
+from wibench.config_loader import load_pipeline_config_yaml
+from wibench.config import PipeLineConfig
+
+
+REEXEC_DONE = "_REEXEC_DONE"
+
+
+def get_config_path_from_argv():
+    argv = sys.argv
+    for i, arg in enumerate(argv):
+        if arg in ("--config", "-c") and i + 1 < len(argv):
+            return argv[i + 1]
+        if arg.startswith("--config="):
+            return arg.split("=", 1)[1]
+    return None
+
+
+def setup_cuda_visible_devices():
+    if os.environ.get(REEXEC_DONE) == "1":
+        return
+
+    config_path = get_config_path_from_argv()
+    if config_path is None:
+        return
+
+    config = load_pipeline_config_yaml(config_path)
+    pipeline_config = config["pipeline"]
+    pipeline_config: PipeLineConfig
+    cuda_devices = pipeline_config.cuda_visible_devices
+
+    if cuda_devices:
+        os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, cuda_devices))
+
+    os.environ[REEXEC_DONE] = "1"
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+
+
+setup_cuda_visible_devices()
+
+
+import wibench
 
 
 CHILD_NUM_ENV_NAME = "WIBENCH_CHILD_PROCESS_NUM"
