7198c9340bcd211dab4c2889e2eb73e0d8b2cafe
Tokarev Sergey <stokarev@ispras.ru>
2026-03-06 13:14:38 +0300

    fix: vine review

--- src/wibench/algorithms/vine/wrapper.py
+++ src/wibench/algorithms/vine/wrapper.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 from dataclasses import dataclass
 import sys
-from typing import Any, Optional, Union
+from typing import Any, Dict, Optional, Union
 import torch
 
-from submodules.VINE.vine.src.stega_encoder_decoder import CustomConvNeXt
-from submodules.VINE.vine.src.vine_turbo import VINE_Turbo
-
+from wibench.module_importer import ModuleImporter
 from wibench.algorithms.base import BaseAlgorithmWrapper
 from wibench.config import Params
 from wibench.typing import TorchImg, TorchImgNormalize
@@ -14,20 +12,44 @@ from wibench.utils import normalize_image, denormalize_image, resize_torch_img
 from wibench.watermark_data import TorchBitWatermarkData
 
 
+DEFAULT_MODULE_PATH = "./submodules/VINE"
+DEFAULT_ENCODER_PATH = "./model_files/vine/VINE-B-Enc"
+DEFAULT_DECODER_PATH = "./model_files/vine/VINE-B-Dec"
+NAME = "vine"
+
+
 @dataclass
 class VINEParams(Params):
-    encoder_weights_path: Optional[Union[str, Path]] = None
-    decoder_weights_path: Optional[Union[str, Path]] = None
+    encoder_weights_path: Optional[Union[str, Path]] = DEFAULT_ENCODER_PATH
+    decoder_weights_path: Optional[Union[str, Path]] = DEFAULT_DECODER_PATH
     H: int = 256
     W: int = 256
     wm_length: int = 100
 
 
 class VINEWrapper(BaseAlgorithmWrapper):
+    """Robust Watermarking Using Generative Priors Against Image Editing: From Benchmarking to Advances [`paper <https://arxiv.org/abs/2410.18775>`__].
     
-    name = "vine"
+    Provides an interface for embedding and extracting watermarks using the VINE watermarking algorithm.
+    Based on the code from the github `repository <https://github.com/Shilin-LU/VINE>`__.
+    
+    Parameters
+    ----------
+    params : Dict[str, Any]
+        VINE algorithm configuration parameters (default EmptyDict)
+    """
 
-    def __init__(self, params: VINEParams):
+    name = NAME
+
+    def __init__(self, params: Dict[str, Any] = {}):
+        super().__init__(VINEParams(**params))
+        self.params: VINEParams
+        self.device = self.params.device
+        module_path = ModuleImporter.pop_resolve_module_path(params, DEFAULT_MODULE_PATH)
+        with ModuleImporter("VINE", module_path):
+            from VINE.vine.src.stega_encoder_decoder import CustomConvNeXt
+            from VINE.vine.src.vine_turbo import VINE_Turbo
+        
         sys.path.append(str(Path(params["module_path"]).resolve()))
         super().__init__(VINEParams(**params))
         
@@ -39,10 +61,19 @@ class VINEWrapper(BaseAlgorithmWrapper):
         if not decoder_weights_path.exists():
             raise FileNotFoundError(f"The decoder weights path: '{str(decoder_weights_path)}' does not exist!")
 
-        self.encoder = VINE_Turbo.from_pretrained(encoder_weights_path, device=self.params.device)
-        self.decoder = CustomConvNeXt.from_pretrained(decoder_weights_path, device=self.params.device)
+        self.encoder = VINE_Turbo.from_pretrained(encoder_weights_path, device=self.device)
+        self.decoder = CustomConvNeXt.from_pretrained(decoder_weights_path, device=self.device)
 
     def embed(self, image: TorchImg, watermark_data: TorchBitWatermarkData) -> TorchImg:
+        """Embed watermark into input image.
+        
+        Parameters
+        ----------
+        image : TorchImg
+            Input image tensor in (C, H, W) format
+        watermark_data: TorchBitWatermarkData
+            Torch bit message with data type torch.int64
+        """
         normalized_image: TorchImgNormalize = normalize_image(image)
         resized_normalized_image: TorchImgNormalize = resize_torch_img(normalized_image, [self.params.H, self.params.W])
 
@@ -57,6 +88,15 @@ class VINEWrapper(BaseAlgorithmWrapper):
         return encoded_image
 
     def extract(self, image: TorchImg, watermark_data: TorchBitWatermarkData) -> Any:
+        """Extract watermark from marked image.
+        
+        Parameters
+        ----------
+        image : TorchImg
+            Input image tensor in (C, H, W) format
+        watermark_data: TorchBitWatermarkData
+            Torch bit message with data type torch.int64
+        """
         resized_image: TorchImgNormalize = resize_torch_img(image, [self.params.H, self.params.W])
         with torch.no_grad():
             res = self.decoder(resized_image)
