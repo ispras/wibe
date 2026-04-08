5e7a6033a8d002cf09ded1a0021b7f8981773bae
Aleksey Yakushev <yakushev@ispras.ru>
2026-04-08 15:03:28 +0000

    Changed prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,5 @@ git submodule update --init --recursive # installs submodules
 python -m venv .venv                    # installs base venv
 source .venv/bin/activate               # activates base venv
 pip install uv                          # installs uv package manager
-uv sync                                 # installs packages for base venv with uv
+uv pip install -e .                     # installs packages for base venv with uv
 python req.py                           # installs other venvs with required packages
