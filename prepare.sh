edcf3c1d5d33e85800add9e1ed6a4d8a80943156
Tokarev Sergey <stokarev@ispras.ru>
2026-07-15 02:05:53 +0300

    fix: rebranding, now you can run `wibench-venv` instead of `python req.py`

--- prepare.sh
+++ prepare.sh
@@ -8,8 +8,8 @@ rm -rf .venv
 rm -rf "venvs/${WIBENCH_PROFILE:-image}"
 rm -rf uv.lock
 git submodule update --init --recursive # installs submodules
-python3 -m venv .venv                    # installs base venv
+python3 -m venv .venv                   # installs base venv
 source .venv/bin/activate               # activates base venv
 pip install uv                          # installs uv package manager
 uv sync                                 # installs packages for base venv with uv
-python req.py all                           # installs other venvs with required packages
+wibench-venv all                        # installs other venvs with required packages
