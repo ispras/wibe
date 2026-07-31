20edd9281d4980a7acd1a3a1a702ed2d500c9afb
Alexander Akimenkov <a.akimenkov@ispras.ru>
2026-07-31 13:01:37 +0000

    fix

--- src/wibench/attacks/instagramcss_filters/instagramcss_filters.py
+++ src/wibench/attacks/instagramcss_filters/instagramcss_filters.py
@@ -5,7 +5,6 @@ from torchvision.transforms.functional import to_tensor
 
 from wibench.attacks.base import BaseAttack
 from wibench.typing import TorchImg
-import pilgram
 
 
 class InstagramCSSFilters(BaseAttack):
@@ -21,6 +20,7 @@ class InstagramCSSFilters(BaseAttack):
     name = "instagramcss_filters"
 
     def __init__(self, attack: str, module: Optional[str] = None) -> None:
+        import pilgram
         if module not in [None, "css"]:
             raise AttributeError(f"Module {module} not supported in pilgram!")
         self.attack = getattr(pilgram if module is None else getattr(pilgram, module), attack)
