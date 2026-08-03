d3f4d772510732ce746b4c126932979fe102e6fe
Obydenkov Dmitry <obydenkov@ispras.ru>
2026-08-03 18:44:18 +0300

    Fix paths for tests data

--- src/wibench/image/metrics/__init__.py
+++ src/wibench/image/metrics/__init__.py
@@ -1,6 +1,7 @@
 from .aesthetic import Aesthetic
 from .blip import BLIP
 from .clip import CLIPScore
+from .clip_iqa import CLIP_IQA
 from .dreamsim import DreamSim
 from .fid import FID
 from .image_reward import ImageReward
